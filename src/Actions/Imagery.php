257ebac2607bdf0e99f61849db4430c0202eeb47
ChurakovMike <churakovmike@mail.ru>
2020-07-28 13:17:29 +0700

    + add imagery action method

--- src/Actions/Imagery.php
+++ src/Actions/Imagery.php
@@ -14,4 +14,24 @@ use ChurakovMike\Accuweather\Client\RequestApi;
  */
 final class Imagery extends BaseAction
 {
+    /**
+     * Returns links to radar and satellite images by location key.
+     * Imagery searches require a location key.
+     * Please use the Locations API to obtain a location key for your desired location.
+     * Radar images are not available for all locations worldwide at this time.
+     * If radar is not available for your desired location, satellite images are still provided.
+     * @see https://developer.accuweather.com/accuweather-imagery-api/apis/get/imagery/v1/maps/radsat/%7Bresolution%7D/%7BlocationKey%7D
+     *
+     * @param string $resolution
+     * @param int $countryCode
+     * @param string $language
+     * @return \stdClass
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function get(string $resolution, int $countryCode, string $language)
+    {
+        return $this->request->send("imagery/v1/maps/radsat/{$resolution}/{$countryCode}", [
+            'language' => $language,
+        ]);
+    }
 }
