f1c96ff658f0e5777e4fe02580a7aafc49bd769a
ChurakovMike <churakovmike@mail.ru>
2020-08-01 21:17:07 +0700

    + add tropical forecast methods

--- src/Actions/Tropical.php
+++ src/Actions/Tropical.php
@@ -14,4 +14,58 @@ use ChurakovMike\Accuweather\Client\RequestApi;
  */
 final class Tropical extends BaseAction
 {
+    /****************************************************
+     *                   Search                         *
+     ***************************************************/
+
+    /****************************************************
+     *                   Position                       *
+     ***************************************************/
+
+    /****************************************************
+     *                   Forecast                       *
+     ***************************************************/
+
+    /**
+     * Returns all recorded forecast information for a specific government-issued tropical cyclone.
+     * @see https://developer.accuweather.com/accuweather-tropical-api/apis/get/tropical/v1/gov/storms/%7Byyyy%7D/%7BbasinID%7D/%7BgovernmentID%7D/forecasts
+     *
+     * @param string $year
+     * @param int $basinId
+     * @param int $governmentId
+     * @param bool $details
+     * @param bool $geometry
+     * @param bool $windowGeometry
+     * @return \stdClass
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function getGovernmentForecast(
+        string $year,
+        int $basinId,
+        int $governmentId,
+        bool $details = false,
+        bool $geometry = false,
+        bool $windowGeometry = false
+    ) {
+        return $this->request->send("tropical/v1/gov/storms/{$year}/{$basinId}/{$governmentId}/forecasts", [
+            'details' => $details,
+            'radiigeometry' => $geometry,
+            'windowgeometry' => $windowGeometry,
+        ]);
+    }
+
+    /**
+     * Returns all recorded forecast information for a specific tropical cyclone.
+     * @see https://developer.accuweather.com/accuweather-tropical-api/apis/get/tropical/v1/storms/%7Byyyy%7D/%7BbasinId%7D/%7BdepressionId%7D/forecasts
+     *
+     * @param string $year
+     * @param int $basinId
+     * @param int $depressionId
+     * @return \stdClass
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function getCycloneForecast(string $year, int $basinId, int $depressionId)
+    {
+        return $this->request->send("tropical/v1/storms/{$year}/{$basinId}/{$depressionId}/forecasts");
+    }
 }
