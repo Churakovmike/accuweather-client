083b324481813759fe2ce8568aac6613cc31ca69
ChurakovMike <churakovmike@mail.ru>
2020-08-03 00:02:52 +0700

    :sparkle: + add poi enums

---
+++ src/Enums/PointOfInterest.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace ChurakovMike\Accuweather\src\Enums;
+
+use MyCLabs\Enum\Enum;
+
+/**
+ * Class PointOfInterest.
+ * @package ChurakovMike\Accuweather\src\Enums
+ */
+class PointOfInterest extends Enum
+{
+    private const AMUSEMENT_PARK = 1;
+    private const AQUARIUM = 2;
+    private const ART = 3;
+    private const BALLOON = 4;
+    private const STADIUM = 5;
+    private const BOTANICAL = 6;
+    private const BREWERY = 7;
+    private const WINERY = 8;
+    private const VINEYARD = 9;
+    private const DISTILLERY = 10;
+    private const BUILDING = 11;
+    private const CASINO = 12;
+    private const DRIVING_RANGE = 13;
+    private const EMBASSY = 14;
+    private const OBSERVATORY = 15;
+    private const PERFORMING_ARTS = 16;
+    private const PLANETARIUM = 17;
+    private const RESORT = 18;
+    private const SHOPPING = 19;
+    private const ZOO = 20;
+    private const TENNIS = 21;
+    private const SPORTS = 22;
+    private const RACING = 23;
+    private const POLO = 24;
+    private const SKI = 25;
+    private const CAMP = 26;
+    private const COUNTY_PARK = 27;
+    private const NATIONAL_PARK = 28;
+    private const NATURE_PRESERVE = 29;
+    private const STATE_PARK = 30;
+    private const WATERFALL = 31;
+    private const Bridge = 32;
+    private const HISTORIC = 33;
+    private const MONUMENT_STATUE = 34;
+    private const MUSEUM = 35;
+    private const STATE_CAPITOL = 36;
+    private const RANDOM = 37;
+    private const AIRPORTS = 38;
+    private const OLYMPICS = 39;
+}
